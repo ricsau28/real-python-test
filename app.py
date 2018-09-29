5447d3885c7f3a37e435ed92c8e44ee145040bf1
ricsau <ricsau@example.con>
2018-09-29 10:54:20 -0400

    Added requirements

--- app.py
+++ app.py
@@ -7,5 +7,32 @@ app = Flask(__name__)
 def hello_world():
     return "Hello, world"
 
+@app.route("/test/<search_query>")
+def search(search_query):
+    return search_query
+
+@app.route("/integer/<int:value>")
+def int_type(value):
+    print(value + 1)
+    return "correct"
+
+@app.route("/float/<float:value>")
+def float_type(value):
+    print(value + 1)
+    return "correct"
+
+# dynamic route that accepts slashes
+@app.route("/path/<path:value>")
+def path_type(value):
+    print(value)
+    return "correct"    
+
+@app.route("/name/<name>")
+def index(name):
+    if name.lower() != "bogeyman":
+        return "Hello, {}".format(name), 200
+    else:
+        return "Not Found", 404
+
 if __name__ == "__main__":
     app.run()
